3bd644b8f7fc88f8a3fd320147b7c3583a318598
Devon Wigle <devonwigle@gmail.com>
2022-03-04 11:25:21 -0600

    Add link back to all hand signals

--- src/Components/HandSignal/HandSignal.js
+++ src/Components/HandSignal/HandSignal.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './HandSignal.scss'
 
 const HandSignal = (props) => {
   console.log(props.chosenSignal[0].name)
   return (
     <div>
+      <Link to={'/'}><h2>All Ref Signals</h2></Link>
       <h2>{`${props.chosenSignal[0].name}`}</h2>
       <img src={`${props.chosenSignal[0].image} alt=${props.chosenSignal[0].name}`} />
       <p>{`${props.chosenSignal[0].use}`}</p>
