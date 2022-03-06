f73db358bf0592b3f5dfbcfb348d73e6df452133
Devon Wigle <devonwigle@gmail.com>
2022-03-06 15:06:59 -0600

    Adjust router to accomodate single api

--- src/Components/HandSignal/HandSignal.js
+++ src/Components/HandSignal/HandSignal.js
@@ -1,24 +1,51 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types';
 import './HandSignal.scss'
+import { getSingleHandSignal } from '../../apiCalls';
+import BadUrl from '../BadUrl/BadUrl';
 
-const HandSignal = (props) => {
-  return (
-    <div key={props.id + "single"}>
-      <h2 data-testid="chosen-title" className='chosen-title'>{`${props.chosenSignal[0].name}`}</h2>
-      <img data-testid="chosen-image" src={`${props.chosenSignal[0].image} alt=${props.chosenSignal[0].name}`} />
-      <p data-testid="use-title" className='chosen-title'>Use:</p>
-      <p data-testid="use-description" className='chosen-description'>{`${props.chosenSignal[0].use}`}</p>
-      <p data-testid="motion-title" className='chosen-title'>Motion:</p>
-      <p data-testid="motion-description" className='chosen-description'>{`${props.chosenSignal[0].motion}`}</p>
-      <Link to={'/'}><h2 onClick={(event) => props.clearFilter(event)} className='link-to-all'>Back to All Referee Signals</h2></Link>
-    </div>
-  )
+class HandSignal extends Component {
+  constructor () {
+    super()
+    this.state = {
+      signal: undefined,
+    }
+  }
+  componentDidMount() {
+    getSingleHandSignal(this.props.id)
+      .then((data) => {
+        this.setState({ signal: data })
+      })
+  }
+
+  render() {
+    console.log(this.state.signal)
+    if (!this.state.signal) {
+      return <BadUrl />
+    }
+    return (
+      <div key={this.state.signal.id + "single"}>
+        <h2 data-testid="chosen-title" className='chosen-title'>{`${this.state.signal.name}`}</h2>
+        <img data-testid="chosen-image" src={`${this.state.signal.image} alt=${this.state.signal.name}`} />
+        <p data-testid="use-title" className='chosen-title'>Use:</p>
+        <p data-testid="use-description" className='chosen-description'>{`${this.state.signal.use}`}</p>
+        <p data-testid="motion-title" className='chosen-title'>Motion:</p>
+        <p data-testid="motion-description" className='chosen-description'>{`${this.state.signal.motion}`}</p>
+        <Link to={{pathname: "https://rules.wftda.com/"}}>See rules</Link>
+        <Link to={'/'}><h2 onClick={(event) => this.props.clearFilter(event)} className='link-to-all'>Back to All Referee Signals</h2></Link>
+      </div>
+    )
+  }
 }
 
 export default HandSignal;
 
 HandSignal.propTypes = {
-  chosenSignal: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired
+  id: PropTypes.string,
+  name: PropTypes.string,
+  use: PropTypes.string,
+  image: PropTypes.string,
+  motion: PropTypes.string,
+  clearFilter: PropTypes.func
 }
